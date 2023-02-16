e7e81831b992892d66c569f450388fc19de94658
tuantuangui <33215039+tuantuangui@users.noreply.github.com>
2023-02-16 13:47:55 +0800

    Add files via upload

--- R/PathwayAnalysis.R
+++ R/PathwayAnalysis.R
@@ -19,17 +19,17 @@ PathwayAnalysis <- function(name,out="Extended",p_cutoff=0.05,noverlap_cutoff=0)
   if (out=="Extended") {
       PathwayExtendData <- PathwayExtendData %>%
         dplyr::select(name,kegg_pathwayname,kegg_category)
-      result <- xgr(name,PathwayExtendData,p_cutoff=p_cutoff,noverlap_cutoff=0)
+      result <- xgr(name,PathwayExtendData,p_cutoff=p_cutoff,noverlap_cutoff= noverlap_cutoff)
   }else if (out=="gene") {
     PathwayExtendData <- PathwayExtendData %>%
       dplyr::filter(type=="gene") %>%
       dplyr::select(name,kegg_pathwayname,kegg_category)
-    result <- xgr(name,PathwayExtendData,p_cutoff=p_cutoff,noverlap_cutoff=0)
+    result <- xgr(name,PathwayExtendData,p_cutoff=p_cutoff,noverlap_cutoff=noverlap_cutoff)
   }else if (out=="metabolite") {
     PathwayExtendData <- PathwayExtendData %>%
       dplyr::filter(type=="metabolite") %>%
       dplyr::select(name,kegg_pathwayname,kegg_category)
-    result <- xgr(name,PathwayExtendData,p_cutoff=p_cutoff,noverlap_cutoff=0)
+    result <- xgr(name,PathwayExtendData,p_cutoff=p_cutoff,noverlap_cutoff=noverlap_cutoff)
   }
   
   result_temp <- result$output
