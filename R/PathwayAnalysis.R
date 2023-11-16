554b67d34a5d6ef8970fbeb8f1ce1ec92bc6f50e
Tuantuan Gui <33215039+tuantuangui@users.noreply.github.com>
2023-11-16 21:31:12 +0800

    Add files via upload

--- R/PathwayAnalysis.R
+++ R/PathwayAnalysis.R
@@ -1,13 +1,15 @@
-#' the KEGG pathway analysis which includes the extended pathway analysis of gene and metabolites
+#' KEGG pathway analysis
 #'
-#' @param name The genes' or the metabolites' names which to analysis pathway
+#' the KEGG pathway analysis which not only includes gene or metabolites pathway analysis alone, but also includes the extended pathway analysis including genes and metabolites
+#'
+#' @param name a vector of genes' or the metabolites' names
 #' @param out The pathway type for gene or metabolite,or extended pathway included genes and metabolites,default is "extended",alternative is "metabolite" and "gene"
 #' @param p_cutoff p_cutoff used to declare the significant terms. By default, it is set to 0.05
 #' @param noverlap_cutoff noverlap_cutoff used to declare the number of overlap. By default, it is set to 0
 #' @param test the test statistic used. It can be "fisher" for using fisher's exact test, "hypergeo" for using hypergeometric test, or "binomial" for using binomial test. Fisher's exact test is to test the independence between gene group (genes belonging to a group or not) and gene annotation (genes annotated by a term or not), and thus compare sampling to the left part of background (after sampling without replacement). Hypergeometric test is to sample at random (without replacement) from the background containing annotated and non-annotated genes, and thus compare sampling to background. Unlike hypergeometric test, binomial test is to sample at random (with replacement) from the background with the constant probability. In terms of the ease of finding the significance, they are in order: hypergeometric test > fisher's exact test > binomial test. In other words, in terms of the calculated p-value, hypergeometric test < fisher's exact test < binomial test
 #'
 #'
-#' @return test
+#' @return result
 #' @export
 #'
 #' @examples
@@ -61,10 +63,10 @@ PathwayAnalysis <- function(name,out="Extended",p_cutoff=0.05,noverlap_cutoff=0,
   result$output <- result_final
 
   kegg_pathway_uniq <- PathwayExtendData %>%
-  dplyr::select(kegg_pathwayname,kegg_category) %>%
-  dplyr::rename("PATHWAY"="kegg_pathwayname") %>%
-  dplyr::rename("pathway_type"="kegg_category") %>%
-  unique()
+    dplyr::select(kegg_pathwayname,kegg_category) %>%
+    dplyr::rename("PATHWAY"="kegg_pathwayname") %>%
+    dplyr::rename("Pathway Category"="kegg_category") %>%
+    unique()
 
   result_1 <- result$output %>%
     dplyr::filter(pvalue < p_cutoff) %>%
@@ -72,12 +74,12 @@ PathwayAnalysis <- function(name,out="Extended",p_cutoff=0.05,noverlap_cutoff=0,
     dplyr::left_join(kegg_pathway_uniq,by=c("name"="PATHWAY"))
 
   result_1$name <- factor(result_1$name,levels = rev(result_1$name))
-  result_1$pathway_type <- factor(result_1$pathway_type,levels=unique(kegg_pathway_uniq$pathway_type))
+  result_1$`Pathway Category` <- factor(result_1$`Pathway Category`,levels=unique(kegg_pathway_uniq$`Pathway Category`))
 
   p1 <- ggplot(result_1,aes(name,-log10(pvalue)))+
-    geom_bar(stat="identity",aes(fill=pathway_type))+
+    geom_bar(stat="identity",aes(fill=`Pathway Category`))+
     scale_fill_manual(values=RColorBrewer::brewer.pal(11, "Set3"),
-                      breaks=unique(kegg_pathway_uniq$pathway_type))+
+                      breaks=unique(kegg_pathway_uniq$`Pathway Category`))+
     coord_flip()+
     theme_bw()+
     labs(x=NULL)
