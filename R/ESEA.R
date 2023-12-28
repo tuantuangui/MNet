02141707ebb955af5eb89e3bce45bdec835f8fda
Tuantuan Gui <33215039+tuantuangui@users.noreply.github.com>
2023-12-28 17:15:44 +0800

    Add files via upload

---
+++ R/ESEA.R
@@ -0,0 +1,28 @@
+#' the ESEA analysis
+#'
+#' the Extended-data Set Enrichment Analysis which includes gene and metabolite
+#'
+#' @param Ranks_all the Named vector of compound-level stats or gene-level stats. Names should be KEGG ID and (or) gene symbol.
+#' @param gseaParam GSEA parameter value, all compound-level and(or) gene-level stats are raised to the power of 'gseaParam' before calculation of GSEA enrichment scores.
+#' @param minSize Minimal size of a compound and(or) gene set to test. All pathways below the threshold are excluded.
+#' @param out The pathway type for gene or metabolite,or extended pathway included genes and metabolites,default is "Extended",alternative is "metabolite" and "gene"
+#'
+#' @return the ESEA result
+#' @export
+#'
+#' @examples
+#' library(ggplot2)
+#' library(dplyr)
+#' result <- ESEA(sim.cpd.data,out="Extended")
+ESEA <- function(Ranks_all,minSize = 5,nPermSimple=20000,gseaParam=0.5,out="extended") {
+  Ranks_all <- sort(Ranks_all)
+
+  if (out=="Extended") {
+    fgseaRes_all <- fgsea::fgsea(Pathways_all, Ranks_all,minSize = minSize,nPermSimple=nPermSimple,gseaParam=gseaParam)
+  }else if (out=="gene") {
+    fgseaRes_all <- fgsea::fgsea(Pathways_gene, Ranks_all,minSize = minSize,nPermSimple=nPermSimple,gseaParam=gseaParam)
+  }else if (out=="metabolite") {
+    fgseaRes_all <- fgsea::fgsea(Pathways_metabolite, Ranks_all,minSize = minSize,nPermSimple=nPermSimple,gseaParam=gseaParam)
+  }
+  return(fgseaRes_all)
+}
